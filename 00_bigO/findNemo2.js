64df704652007d269f3bb538903b4a89bf706dee
sithea-nou <sitheanou@gamil.com>
2025-02-12 17:29:01 +0100

    Add findNemo

---
+++ 00_bigO/findNemo2.js
@@ -0,0 +1,15 @@
+const nemo = ['nemo']
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+
+function findNemo2(array) {
+    let t0 = performance.now();
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 'nemo') {
+            console.log('Found NEMO!');
+        }
+    }
+    let t1 = performance.now();
+    console.log('call to find Nemo took '+ (t1-t0) + ' milliseconds...');
+}
+
+findNemo2(everyone)
