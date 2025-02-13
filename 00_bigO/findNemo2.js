f6db2d83cc6d9b275379266609f861a5851bb235
sithea-nou <sitheanou@gamil.com>
2025-02-13 08:58:38 +0100

    update findNemo2

--- 00_bigO/findNemo2.js
+++ 00_bigO/findNemo2.js
@@ -1,15 +1,15 @@
 const nemo = ['nemo']
 const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
-
+const large = new Array(100).fill('nemo');
 function findNemo2(array) {
-    let t0 = performance.now();
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === 'nemo') {
-            console.log('Found NEMO!');
-        }
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO!');
     }
-    let t1 = performance.now();
-    console.log('call to find Nemo took '+ (t1-t0) + ' milliseconds...');
+  }
+  let t1 = performance.now();
+  console.log('call to find Nemo took ' + (t1 - t0) + ' milliseconds...');
 }
 
-findNemo2(everyone)
+findNemo2(large)
